73bc41c063e3c8d1c251d29fcbd8640a2950a17a
Ben Hatfield <ben.hatfield9@gmail.com>
2020-01-29 13:17:04 -0500

    drop data base and changed test user for admin tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe User, type: :model do
 
   describe "#admin?" do
     it "is not an admin if the role is not admin" do
-      user = User.create(email: "yoyo@gmail.com", username: "yoyo", profile_photo: "photo.png", role: "member")
+      user = User.create(email: "yoyo@gmail.com", username: "yoyo", role: "member")
       expect(user.admin?).to eq(false)
     end
 
     it "is an admin if the role is admin" do
-      user = User.create(email: "yoyo@gmail.com", username: "yoyo", profile_photo: "photo.png", role: "admin")
+      user = User.create(email: "yoyo@gmail.com", username: "yoyo", role: "admin")
       expect(user.admin?).to eq(true)
     end
   end
