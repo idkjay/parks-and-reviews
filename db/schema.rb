0fe07d40f7cd9a2abd09cb240469144571ffd1fe
Ben Hatfield <ben.hatfield9@gmail.com>
2020-01-29 10:57:13 -0500

    to be merged after carrierwave, admin path and tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2020_01_28_151501) do
-=======
-ActiveRecord::Schema.define(version: 2020_01_27_195533) do
->>>>>>> origin
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe User, type: :model do
 
   describe "#admin?" do
     it "is not an admin if the role is not admin" do
-      user = FactoryBot.create(:user, role: "member")
+      user = User.create(email: "yoyo@gmail.com", username: "yoyo", profile_photo: "photo.png", role: "member")
       expect(user.admin?).to eq(false)
     end
 
     it "is an admin if the role is admin" do
-      user = FactoryBot.create(:user, role: "admin")
+      user = User.create(email: "yoyo@gmail.com", username: "yoyo", profile_photo: "photo.png", role: "admin")
       expect(user.admin?).to eq(true)
     end
   end
