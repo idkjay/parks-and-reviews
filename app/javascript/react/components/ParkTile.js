6a35b7c5f18abf1357e9ce07c3887cc35f267786
Hannah Horobin <horobinhl@gmail.com>
2020-01-29 17:02:29 -0500

    Added div in park tile to account for CSS on larger screens

--- app/javascript/react/components/ParkTile.js
+++ app/javascript/react/components/ParkTile.js
@@ -5,13 +5,15 @@ const ParkTile = props => {
   return (
     <div className="row" align="center">
       <div className="columns small-10">
-        <div className="image-wrapper overlay-fade-in">
-          <img className="photo" src={props.photo}/>
+        <div>
+          <div className="image-wrapper overlay-fade-in">
+            <img className="photo" src={props.photo}/>
             <div className="image-overlay-content">
               <h2 id="name">{props.name}</h2>
               <p id="location">{props.city}, {props.state}</p>
               <p id="rating">{props.rating} stars</p>
             </div>
+          </div>
         </div>
       </div>
     </div>
